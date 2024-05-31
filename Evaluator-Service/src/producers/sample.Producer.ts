5dc8ce3753ca509633001cb62f59afcb7bd7fd98
khush <khush.suthar@gmail.com>
2024-05-31 08:40:38 +0530

    Fastify setup

--- Evaluator-Service/src/producers/sample.Producer.ts
+++ Evaluator-Service/src/producers/sample.Producer.ts
@@ -3,3 +3,4 @@ import sampleQueues from "../queues/sample.Queues";
 export default async function(name:string, payload: Record<string,unknown>, priority:number) {
     await sampleQueues.add(name, payload, {priority});
 } 
+
